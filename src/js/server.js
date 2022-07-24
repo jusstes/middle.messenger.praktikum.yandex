3e670070c5be1140ee05872f8bce84aafb175092
Aleksey Golodnikov <jusstes@yandex.ru>
2022-07-24 13:26:18 +0300

    feat: добавить переменные окружения

--- src/js/server.js
+++ src/js/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static(`./dist/`));
 
